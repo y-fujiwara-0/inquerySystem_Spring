54618251a626a619015ac8e369f8e9eebdc080da
aoisuzumura <a-suzumura@zyyx.jp>
2024-05-29 17:42:28 +0900

    初期データ内容変更

--- poco-tech-spring-boot-54fa10ab734c2e087d7a61e9ee850b76a7377f11/src/main/resources/data.sql
+++ poco-tech-spring-boot-54fa10ab734c2e087d7a61e9ee850b76a7377f11/src/main/resources/data.sql
@@ -1,5 +1,5 @@
 insert into inquerys (mailAddress, name, old, address, classification, day, unread, body)
-values ('a-suzumura+01@zyyx.jp', 'test', '25', '大阪府大阪市西区江戸堀１−９−１', '1', '2023-05-30', '1','問い合わせシステムテスト用文章です。');
+values ('a-suzumura+01@zyyx.jp', 'test', '25', '大阪府大阪市西区江戸堀１−９−１', '1', '2021-05-30', '0','問い合わせシステムテスト用文章です。');
 insert into inquerys (mailAddress, name, old, address, classification, day, unread, body)
 values ('suzu825193@gmail.com', 'aoi', '24', '愛知県春日井市岩成台１−５−１２', '2', '2022-02-11', '1', '問い合わせシステムテスト文章です。order')
 
