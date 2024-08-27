03e64567c3eb01b3f4b8d71e62b273062e3b82dd
aoisuzumura <a-suzumura@zyyx.jp>
2024-08-27 11:13:03 +0900

    ファイル構成変更

--- poco-tech-spring-boot-54fa10ab734c2e087d7a61e9ee850b76a7377f11/src/main/java/com/example/its/domain/mail/MailRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.its.domain.mail;
-
-import org.springframework.mail.MailSender;
-
-public interface MailRepository {
-   MailSender sendMail(String mailaddress,String Subject,String Text);
-}
